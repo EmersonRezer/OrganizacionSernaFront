5a641d0acf81571a46ac8b01ebd313741d910504
EmersonUAEH <ri414194@uaeh.edu.mx>
2023-07-24 13:26:35 -0600

    Todo listo para empezar

---
+++ config/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.dev.com',
+  variableMuestra: 'esta es mi variable de entorno'
+};
--- src/app/Componentes/inicio/inicio.component.ts
+++ src/app/Componentes/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {environment} from "../../../../config/environment";
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+
+  muestra = environment.variableMuestra;
 
 }
